93ba51f127c887fe00e9b45efae6144df35c1c42
Daniil Popov <popovdanin@gmail.com>
2025-02-14 11:45:50 +0300

    Initial commit

---
+++ src/test/java/org/skyhigh/uninotesservice/UniNotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyhigh.uninotesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniNotesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
